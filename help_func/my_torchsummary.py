Fix calculation when output type is list

help_func/my_torchsummary.py
@@ -118,7 +118,11 @@ def summary(model, input_size, batch_size=-1, device="cuda"):
             "{0:,}".format(summary[layer]["nb_params"]),
         )
         total_params += summary[layer]["nb_params"]
-        total_output += np.prod(summary[layer]["output_shape"])
+        if type(summary[layer]["output_shape"][0]) is list:
+            for output in summary[layer]["output_shape"]:
+                total_output += np.prod(output)
+        else:
+            total_output += np.prod(summary[layer]["output_shape"])
         if "trainable" in summary[layer]:
             if summary[layer]["trainable"] == True:
                 trainable_params += summary[layer]["nb_params"]